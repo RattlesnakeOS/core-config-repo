5d3ee5b00a9a1657066795dd2b3b310bc59ab71e
Dan Vittegleo <dan@vittegleo.com>
2022-02-05 17:36:36 -0800

    add adevtool/vendor state

--- hooks/aosp_local_repo_additions_post.sh
+++ hooks/aosp_local_repo_additions_post.sh
@@ -2,10 +2,12 @@
 
 # TODO: apv workaround - remove once alternative is built
 mkdir -p "${AOSP_BUILD_DIR}/.repo/local_manifests"
-cat <<EOF >> "${AOSP_BUILD_DIR}/.repo/local_manifests/apv.xml"
+cat <<EOF >> "${AOSP_BUILD_DIR}/.repo/local_manifests/vendor.xml"
 <?xml version="1.0" encoding="UTF-8"?>
 <manifest>
-  <remote name="vendor" fetch="${APV_REMOTE}" revision="${APV_BRANCH}" />
-  <project path="vendor/android-prepare-vendor" name="android-prepare-vendor" remote="vendor" revision="${APV_REVISION}" />
+  <remote name="apv" fetch="${APV_REMOTE}" revision="${APV_BRANCH}" />
+  <project path="vendor/adevtool" name="adevtool" remote="rattlesnakeos" />
+  <project path="vendor/state" name="vendor_state" remote="rattlesnakeos" />
+  <project path="vendor/android-prepare-vendor" name="android-prepare-vendor" remote="apv" revision="${APV_REVISION}" />
 </manifest>
 EOF
